bd3be7d07a9faa59715b5c180de59a1cb046fa77
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-01-04 18:37:44 +0000

    Create navbar and setup routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
-      <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
-        <Route path='signup' element={() => {}}/>
-        <Route path='signin' element={() => {}}/>
-      </Routes>
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/signup" element={<h1>signup</h1>} />
+          <Route path="/signin" element={() => {}} />
+        </Routes>
+      </Container>
     </div>
   );
 }
